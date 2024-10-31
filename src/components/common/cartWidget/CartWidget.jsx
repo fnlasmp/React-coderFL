0a3b238c664296bdf5ed5715d5f0474f03184693
Fabricio Lopez <fnla0911@gmail.com>
2024-10-31 03:05:48 -0300

    Context, Cart, CartContainer

--- src/components/common/cartWidget/CartWidget.jsx
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -1,10 +1,15 @@
 import LocalMallIcon from "@mui/icons-material/LocalMall";
 import Badge from "@mui/material/Badge";
+import { useContext } from "react";
+import { CartContext } from "../../../context/CartContext";
+import Cart from "../../pages/cart/CartContainer";
 
 const CartWidget = () => {
+  const { cart } = useContext(CartContext); // siempre devuelve el value del context
+
   return (
     <div>
-      <Badge badgeContent={0} color="secondary">
+      <Badge badgeContent={cart.length} color="secondary">
         <LocalMallIcon />
       </Badge>
     </div>
